c571b743c6ab8bf816eaff126b328d85fb508fd8
Sebastian Nozzi <sebnozzi@gmail.com>
2015-09-20 21:02:14 +0200

    Reading form/text input - Not submitting yet

--- client/src/main/scala/example/ScalaJSExample.scala
+++ client/src/main/scala/example/ScalaJSExample.scala
@@ -4,8 +4,23 @@ import scala.scalajs.js
 import org.scalajs.dom
 import shared.SharedMessages
 
+import org.scalajs.jquery._
+
 object ScalaJSExample extends js.JSApp {
+
   def main(): Unit = {
     dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
+
+    def onFormSubmit(ev: JQueryEventObject):js.Any = {
+      val msg = jQuery("#msg").value()
+      println(s"This should be sent to server: $msg")      
+      jQuery("#msg").value("")
+      jQuery("#msg").focus()
+      false // don't actually submit the form
+    }
+    
+    jQuery("#sendForm").submit(onFormSubmit _)
+    jQuery("#msg").focus()
   }
+
 }
